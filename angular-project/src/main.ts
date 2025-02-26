8c417a86311e6eb6175b8af1d33fb62032a2a1ee
victsilva37 <victormau.sm@gmail.com>
2025-02-26 16:58:48 -0300

    Se configuran las importaciones para el funcionamiento de la api

--- angular-project/src/main.ts
+++ angular-project/src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(),        // Proveedor para HttpClient
+    provideRouter(routes),      // Proveedor para rutas
+  ],
+}).catch((err) => console.error('Error al iniciar la aplicación:', err));
+

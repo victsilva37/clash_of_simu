2b3e4bdcebcf565db71436b27e227c8ab11249f3
victsilva37 <victormau.sm@gmail.com>
2025-02-28 15:58:24 -0300

    Se crean las funciones para interacción del menú

--- angular-project/src/app/components/menu/menu.component.ts
+++ angular-project/src/app/components/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -7,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
+  constructor(private router: Router) {}
+  goToInicio(): void {
+    const playerData = history.state.playerData; // Recuperar el estado actual del jugador
+    this.router.navigate(['/inicio'], { state: { playerData } });
+  }
 
+  goToSimulaciones(): void {
+    const playerData = history.state.playerData; // Recuperar el estado actual del jugador
+    this.router.navigate(['/simulaciones'], { state: { playerData } });
+  }
 }

4aa4fc584ce9773055b0338f13be4ff09ddc7556
victsilva37 <victormau.sm@gmail.com>
2025-02-28 15:56:09 -0300

    Se agrega la ruta de las simulaciones

--- angular-project/src/app/app.routes.ts
+++ angular-project/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginTagComponent } from './pages/login-tag/login-tag.component';
 import { InicioComponent } from './pages/inicio/inicio.component';
+import { SimulacionesComponent } from './pages/simulaciones/simulaciones.component';
 
 export const routes: Routes = [
     {path: '', component: LoginTagComponent},
-    {path: 'inicio', component: InicioComponent}
+    {path: 'inicio', component: InicioComponent},
+    {path: 'simulaciones', component: SimulacionesComponent},
 ];

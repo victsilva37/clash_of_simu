96dac512c7ad9002e9a40f3e5de3eac63b894548
victsilva37 <victormau.sm@gmail.com>
2025-04-04 12:04:08 -0300

    Se importan las variables de entorno y las tablas

--- angular-project/backend/src/app.module.ts
+++ angular-project/backend/src/app.module.ts
@@ -2,25 +2,28 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PlayersModule } from './players/players.module'; // Asegúrate de importar el módulo de jugadores
 import { Jugador } from './players/players.entity';
-import { TropasDispService } from './tropas_disp/tropas_disp.service';
+import { TropaDispModule } from './tropas_disp/tropas_disp.module';
+import { TropaDisp } from './tropas_disp/tropas_disp.entity';
+import { environment } from './environments/enviroment';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'oracle',
-      host: 'localhost',
-      port: 1521,
-      username: 'HOLA123',
-      password: 'HOLA_12345',
-      serviceName: 'XEPDB1',
-      entities: [Jugador],
-      synchronize: false,
-      migrationsRun: true,
-      logging: true, // Habilita logs SQL
+      type: environment.database.type as any,
+      host: environment.database.host,
+      port: environment.database.port,
+      username: environment.database.username,
+      password: environment.database.password,
+      serviceName: environment.database.serviceName,
+      entities: [Jugador, TropaDisp],
+      synchronize: environment.database.synchronize,
+      migrationsRun: environment.database.migrationsRun,
+      logging: environment.database.logging,
     }),
     PlayersModule,
+    TropaDispModule
   ],
-  providers: [TropasDispService],
+  providers: [],
 })
 export class AppModule {}
 

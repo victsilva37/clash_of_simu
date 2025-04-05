a3db647fa9d6bc3311192beda52f25a9ba6cf7d2
victsilva37 <victormau.sm@gmail.com>
2025-04-04 22:23:25 -0300

    Se borra un archivo innecesario

--- angular-project/backend/src/players/players.repository.ts
+++
@@ -1,7 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm';
-import { Jugador } from './players.entity'
-
-@EntityRepository(Jugador)
-export class JugadorRepository extends Repository<Jugador> {
-  // Métodos adicionales si es necesario
-}
